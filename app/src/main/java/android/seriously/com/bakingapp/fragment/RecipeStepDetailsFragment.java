1fdcd4e076be70a9acd64752d08ea87f62e71c0c
evgeniy-sibagatullin <evgeniy.sibagatullin@gmail.com>
2018-04-03 15:54:41 +0300

    Add Exoplayer to display recipe step videos.

--- app/src/main/java/android/seriously/com/bakingapp/fragment/RecipeStepDetailsFragment.java
+++ app/src/main/java/android/seriously/com/bakingapp/fragment/RecipeStepDetailsFragment.java
@@ -1,6 +1,7 @@
 package android.seriously.com.bakingapp.fragment;
 
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.seriously.com.bakingapp.R;
 import android.seriously.com.bakingapp.databinding.RecipeStepDetailsFragmentBinding;
@@ -12,17 +13,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import static android.seriously.com.bakingapp.utils.Constants.BUNDLE_KEY_RECIPE_STEP;
 import static android.seriously.com.bakingapp.utils.Constants.BUNDLE_KEY_RECIPE_STEP_ID_BEFORE;
 import static android.seriously.com.bakingapp.utils.Constants.BUNDLE_KEY_RECIPE_STEP_ID_NEXT;
 import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
 
 @SuppressWarnings("ConstantConditions")
 public class RecipeStepDetailsFragment extends Fragment {
 
     public static final String TAG = RecipeStepDetailsFragment.class.getSimpleName();
+
+    private ExoPlayer exoPlayer;
+    private MediaSource mediaSource;
+    private RecipeStepDetailsFragmentBinding binding;
 
     public interface Listener {
         void onNavigationButtonPressed(int recipeStepToBeOpenedId);
@@ -36,22 +49,86 @@ public class RecipeStepDetailsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        RecipeStepDetailsFragmentBinding binding = DataBindingUtil.inflate(inflater,
-                R.layout.recipe_step_details_fragment, container, false);
-
+        binding = DataBindingUtil.inflate(inflater, R.layout.recipe_step_details_fragment,
+                container, false);
         recipeStep = (RecipeStep) getArguments().get(BUNDLE_KEY_RECIPE_STEP);
 
-        setupInstruction(binding.recipeStepInstruction);
-        setupNavigationButtons(binding);
+        setupInstruction();
+        setupNavigationButtons();
 
         return binding.getRoot();
     }
 
-    private void setupInstruction(TextView recipeStepInstruction) {
-        recipeStepInstruction.setText(recipeStep.getFullDesc());
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
     }
 
-    private void setupNavigationButtons(RecipeStepDetailsFragmentBinding binding) {
+    @Override
+    public void onResume() {
+        super.onResume();
+        if ((Util.SDK_INT <= 23 || exoPlayer == null)) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+    private void releasePlayer() {
+        if (exoPlayer != null) {
+            exoPlayer.release();
+            exoPlayer = null;
+        }
+
+        if (mediaSource != null) {
+            mediaSource = null;
+        }
+    }
+
+    private void initializePlayer() {
+        String videoUrl = recipeStep.getVideoUrl();
+
+        if (videoUrl == null || videoUrl.isEmpty()) return;
+
+        binding.videoPlayer.setVisibility(VISIBLE);
+
+        if (exoPlayer == null) {
+            DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(getContext(),
+                    Util.getUserAgent(getContext(), getString(R.string.app_name)));
+            mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                    .createMediaSource(Uri.parse(videoUrl));
+
+            exoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), new DefaultTrackSelector());
+            exoPlayer.prepare(mediaSource);
+            exoPlayer.setPlayWhenReady(true);
+
+            binding.videoPlayer.requestFocus();
+            binding.videoPlayer.setPlayer(exoPlayer);
+        }
+    }
+
+    private void setupInstruction() {
+        binding.recipeStepInstruction.setText(recipeStep.getFullDesc());
+    }
+
+    private void setupNavigationButtons() {
         setupNavigationButton(binding.navigateBefore, BUNDLE_KEY_RECIPE_STEP_ID_BEFORE);
         setupNavigationButton(binding.navigateNext, BUNDLE_KEY_RECIPE_STEP_ID_NEXT);
         setupBackToListButton(binding.backToList);

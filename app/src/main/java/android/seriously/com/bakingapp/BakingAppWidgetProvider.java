324d8258468634d7a031c299266a9a2a36d1bb13
evgeniy-sibagatullin <evgeniy.sibagatullin@gmail.com>
2018-04-08 21:21:12 +0300

    Add blank widget.

---
+++ app/src/main/java/android/seriously/com/bakingapp/BakingAppWidgetProvider.java
@@ -0,0 +1,34 @@
+package android.seriously.com.bakingapp;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+public class BakingAppWidgetProvider extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_app_widget_provider);
+
+        CharSequence blankWidgetText = context.getString(R.string.blank_widget_text);
+        views.setTextViewText(R.id.blank_widget_text, blankWidgetText);
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+    }
+}
